e42dd049a55c5690697d48733a00316a838ea338
evsig-b2p <andrey.evsigneev@best2pay.net>
2022-12-07 16:11:24 +0300

    nm update from GL, add purchase_by_qr

--- best2pay.php
+++ best2pay.php
@@ -1,17 +1,20 @@
 <?php
 
 /**
-* @author Dennis Prochko
-* @version 1.1.0
-* @package VirtueMart
-* @subpackage payment
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*
-* Best2Pay payment plugin for VirtueMart 3
-*
-* http://www.best2pay.net
-*
-*/
+ * @author Best2Pay
+ * @version 1.1.0
+ * @package VirtueMart
+ * @subpackage payment
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ *
+ * Best2Pay payment plugin for VirtueMart 3, 4
+ *
+ * http://www.best2pay.net
+ *
+ * tested on Joomla 4.1.5 and VirtueMart 4.0.4
+ */
+
+use Joomla\CMS\Factory;
 
 defined('_JEXEC') or die('Restricted access');
 if (!class_exists('vmPSPlugin')) {
@@ -19,7 +22,7 @@ if (!class_exists('vmPSPlugin')) {
 }
 
 class plgVmPaymentBest2Pay extends vmPSPlugin {
-
+	
 	function __construct(&$subject, $config) {
 		parent::__construct($subject, $config);
 		$this->_loggable = TRUE;
@@ -37,43 +40,40 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		);
 		$this->setConfigParameterable($this->_configTableFieldName, $varsToPush);
 	}
-
+	
 	public function getTableSQLFields() {
-		$SQLfields = array(
-            'id' => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT',
-            'virtuemart_order_id' => 'int(1) UNSIGNED',
-            'order_number' => 'char(64)',
-            'virtuemart_paymentmethod_id' => 'mediumint(1) UNSIGNED',
-            'payment_name' => 'varchar(5000)',
-            'payment_order_total' => 'decimal(15,5) NOT NULL DEFAULT \'0.00000\'',
-            'payment_currency' => 'char(3)',
+		return array(
+			'id' => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT',
+			'virtuemart_order_id' => 'int(1) UNSIGNED',
+			'order_number' => 'char(64)',
+			'virtuemart_paymentmethod_id' => 'mediumint(1) UNSIGNED',
+			'payment_name' => 'varchar(5000)',
+			'payment_order_total' => 'decimal(15,5) NOT NULL DEFAULT \'0.00000\'',
+			'payment_currency' => 'char(3)',
 			'cost_per_transaction' => 'decimal(10,2)',
 			'cost_percent_total' => 'decimal(10,2)',
 			'tax_id' => 'smallint(1)',
 			'user_session' => 'varchar(255)'
 		);
-		return $SQLfields;
 	}
-
+	
 	public function getVmPluginCreateTableSQL() {
 		return $this->createTableSQL('Best2Pay Table');
 	}
-
+	
 	protected function displayLogos($logo_list) {
-		$img = "";
-
-		if (!(empty($logo_list))) {
-			$url = JURI::root() . str_replace('\\', '/', str_replace(JPATH_ROOT, '', dirname(__FILE__))) . '/';
-			if (!is_array($logo_list))
-				$logo_list = (array) $logo_list;
-			foreach ($logo_list as $logo) {
-				$alt_text = substr($logo, 0, strpos($logo, '.'));
-				$img .= '<img align="middle" src="' . $url . $logo . '"  alt="' . $alt_text . '" /> ';
-			}
+		$img = '';
+		if(!$logo_list) return $img;
+		$url = JURI::root() . str_replace('\\', '/', str_replace(JPATH_ROOT, '', dirname(__FILE__))) . '/';
+		if (!is_array($logo_list))
+			$logo_list = (array) $logo_list;
+		foreach ($logo_list as $logo) {
+			$alt_text = substr($logo, 0, strpos($logo, '.'));
+			$img .= '<img align="middle" src="' . $url . $logo . '"  alt="' . $alt_text . '" /> ';
 		}
 		return $img;
 	}
-
+	
 	function plgVmConfirmedOrder($cart, $order) {
 		if (!($method = $this->getVmPluginMethod($order['details']['BT']->virtuemart_paymentmethod_id))) {
 			return NULL;
@@ -83,12 +83,12 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		}
 		return $this->sendTransactionRequest($method,$cart, $order);
 	}
-
+	
 	function sendTransactionRequest($method, $cart, $order) {
 		$lang = JFactory::getLanguage ();
 		$filename = 'com_virtuemart';
 		$lang->load($filename, JPATH_ADMINISTRATOR);
-
+		
 		if (!class_exists('VirtueMartModelOrders')) {
 			require(JPATH_VM_ADMINISTRATOR . DS . 'models' . DS . 'orders.php');
 		}
@@ -98,21 +98,21 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		if (!class_exists ('CurrencyDisplay')) {
 			require(JPATH_VM_ADMINISTRATOR . DS . 'helpers' . DS . 'currencydisplay.php');
 		}
-
+		
 		$this->getPaymentCurrency($method);
 		$currency_code_3 = shopFunctions::getCurrencyByID($method->payment_currency, 'currency_code_3');
 		$currency = shopFunctions::getCurrencyByID($method->payment_currency, 'currency_numeric_code');
-
+		
 		$totalInPaymentCurrency = plgVmPaymentBest2Pay::getAmountInCurrency($order['details']['BT']->order_total,$method->payment_currency);
 		$price = $totalInPaymentCurrency['value'];
-
+		
 		$dbValues['order_number'] = $order['details']['BT']->order_number;
 		$dbValues['virtuemart_paymentmethod_id'] = $order['details']['BT']->virtuemart_paymentmethod_id;
 		$dbValues['payment_name'] = $this->renderPluginName($method) . '<br />' . $method->payment_info;
 		$dbValues['payment_order_total'] = $totalInPaymentCurrency['value'];
 		$dbValues['payment_currency'] = $currency_code_3;
 		$this->storePSPluginInternalData($dbValues);
-
+		
 		if ($method->test_mode == '0')
 			$best2pay_url = 'https://pay.best2pay.net';
 		else
@@ -133,33 +133,36 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 			'signature' => $signature
 		);
 		$options = array(
-		    'http' => array(
-		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		        'method'  => 'POST',
-		        'content' => http_build_query($data),
-		    ),
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data),
+			),
 		);
-
-		$context  = stream_context_create($options);
+		
+		$context = stream_context_create($options);
 		$best2pay_id = file_get_contents($url, false, $context);
-
+		
+		$mainframe = JFactory::getApplication();
 		if (intval($best2pay_id) == 0) {
-			JError::raiseWarning(500, var_export($best2pay_id, true));
-			return false;
+			$mainframe = JFactory::getApplication();
+			$mainframe->enqueueMessage(JText::_('VMPAYMENT_BEST2PAY_ERROR', false), 'warning');
+			$mainframe->enqueueMessage($best2pay_id, 'warning');
+			$mainframe->redirect(JURI::root() . 'index.php?option=com_virtuemart&view=cart');
 		}
-
+		
 		$signature = base64_encode(md5($method->sector_id . $best2pay_id . $method->password));
 		$redirect_url = $best2pay_url
 			. '/webapi/Purchase'
 			. '?sector=' . $method->sector_id
 			. '&id=' . $best2pay_id
 			. '&signature=' . $signature;
-		$mainframe = JFactory::getApplication();
+		
 		$mainframe->redirect($redirect_url);
-
+		
 		return true;
 	}
-
+	
 	function getNewStatus($method) {
 		if (isset($method->status_pending) && $method->status_pending != '') {
 			return $method->status_pending;
@@ -167,7 +170,7 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 			return 'P';
 		}
 	}
-
+	
 	function plgVmOnShowOrderBEPayment($virtuemart_order_id, $virtuemart_payment_id) {
 		if (!$this->selectedThisByMethodId($virtuemart_payment_id)) {
 			return NULL;
@@ -176,7 +179,7 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 			return NULL;
 		}
 		VmConfig::loadJLang('com_virtuemart');
-
+		
 		$html = '<table class="adminlist">' . "\n";
 		$html .= $this->getHtmlHeaderBE();
 		$html .= $this->getHtmlRowBE('COM_VIRTUEMART_PAYMENT_NAME', $paymentTable->payment_name);
@@ -184,31 +187,31 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		$html .= '</table>' . "\n";
 		return $html;
 	}
-
-    function getCosts(VirtueMartCart $cart, $method, $cart_prices) {
-        return 0;
-    }
-
+	
+	function getCosts(VirtueMartCart $cart, $method, $cart_prices) {
+		return 0;
+	}
+	
 	public function checkConditions($cart, $method, $cart_prices) {
 		return true;
 	}
-
+	
 	function plgVmOnStoreInstallPaymentPluginTable($jplugin_id) {
 		return $this->onStoreInstallPluginTable($jplugin_id);
 	}
-
+	
 	public function plgVmOnSelectCheckPayment (VirtueMartCart $cart, &$msg) {
 		return $this->OnSelectCheck($cart);
 	}
-
+	
 	public function plgVmDisplayListFEPayment (VirtueMartCart $cart, $selected = 0, &$htmlIn) {
 		return $this->displayListFE($cart, $selected, $htmlIn);
 	}
-
+	
 	public function plgVmonSelectedCalculatePricePayment (VirtueMartCart $cart, array &$cart_prices, &$cart_prices_name) {
 		return $this->onSelectedCalculatePrice($cart, $cart_prices, $cart_prices_name);
 	}
-
+	
 	function plgVmgetPaymentCurrency($virtuemart_paymentmethod_id, &$paymentCurrencyId) {
 		if (!($method = $this->getVmPluginMethod($virtuemart_paymentmethod_id))) {
 			return NULL;
@@ -219,31 +222,31 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		$this->getPaymentCurrency($method);
 		$paymentCurrencyId = $method->payment_currency;
 	}
-
+	
 	function plgVmOnCheckAutomaticSelectedPayment (VirtueMartCart $cart, array $cart_prices = array()/*, &$paymentCounter*/) {
 		return $this->onCheckAutomaticSelected($cart, $cart_prices/*, $paymentCounter*/);
 	}
-
+	
 	public function plgVmOnShowOrderFEPayment($virtuemart_order_id, $virtuemart_paymentmethod_id, &$payment_name) {
 		$this->onShowOrderFE($virtuemart_order_id, $virtuemart_paymentmethod_id, $payment_name);
 	}
-
+	
 	function plgVmonShowOrderPrintPayment($order_number, $method_id) {
 		return $this->onShowOrderPrint($order_number, $method_id);
 	}
-
+	
 	function plgVmDeclarePluginParamsPayment($name, $id, &$data) {
 		return $this->declarePluginParams ('payment', $name, $id, $data);
 	}
-
+	
 	function plgVmSetOnTablePluginParamsPayment($name, $id, &$table) {
 		return $this->setOnTablePluginParams($name, $id, $table);
 	}
-
+	
 	function plgVmDeclarePluginParamsPaymentVM3( &$data) {
 		return $this->declarePluginParams('payment', $data);
 	}
-
+	
 	static function getAmountInCurrency($amount, $currencyId) {
 		if (method_exists('vmPSPlugin', 'getAmountInCurrency')) {
 			return vmPSPlugin::getAmountInCurrency($amount, $currencyId);
@@ -251,18 +254,19 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		if (!class_exists ('CurrencyDisplay')) {
 			require(JPATH_VM_ADMINISTRATOR . DS . 'helpers' . DS . 'currencydisplay.php');
 		}
- 		$return = array();
+		
+		$return = array();
 		$paymentCurrency = CurrencyDisplay::getInstance($currencyId);
 		$return['value'] = round($paymentCurrency->convertCurrencyTo($currencyId, $amount, false), 2);
 		$return['display'] = $paymentCurrency->priceDisplay($amount, $currencyId);
 		return $return;
 	}
-
+	
 	public function plgVmOnPaymentResponseReceived(&$html) {
 		$lang = JFactory::getLanguage();
 		$filename = 'com_virtuemart';
 		$lang->load($filename, JPATH_ADMINISTRATOR);
-
+		
 		if (!class_exists('shopFunctionsF')) {
 			require(JPATH_VM_SITE . DS . 'helpers' . DS . 'shopfunctionsf.php');
 		}
@@ -278,10 +282,10 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		if (!class_exists('VirtueMartCart')) {
 			require(JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php');
 		}
-
-		$virtuemart_paymentmethod_id = JRequest::getInt('pm', 0);
-		$order_number = JRequest::getString('reference', 0);
-
+		
+		$virtuemart_paymentmethod_id = vRequest::getInt('pm', 0);
+		$order_number = vRequest::getString('reference', 0);
+		
 		if (!($method = $this->getVmPluginMethod($virtuemart_paymentmethod_id))) {
 			return NULL;
 		}
@@ -294,37 +298,37 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		if (!($paymentTables = $this->getDatasByOrderId($virtuemart_order_id))) {
 			return false;
 		}
-
-		$signature = base64_encode(md5($method->sector_id . JRequest::getInt('id', 0) . JRequest::getInt('operation', 0) . $method->password));
-
+		
+		$signature = base64_encode(md5($method->sector_id . vRequest::getInt('id', 0) . vRequest::getInt('operation', 0) . $method->password));
+		
 		if ($method->test_mode == '0')
 			$best2pay_url = 'https://pay.best2pay.net';
 		else
 			$best2pay_url = 'https://test.best2pay.net';
 		$url = $best2pay_url . '/webapi/Operation';
-
+		
 		$context = stream_context_create(array(
 			'http' => array(
 				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
 				'method'  => 'POST',
 				'content' => http_build_query(array(
 					'sector' => $method->sector_id,
-					'id' => JRequest::getInt('id', 0),
-					'operation' => JRequest::getInt('operation', 0),
+					'id' => vRequest::getInt('id', 0),
+					'operation' => vRequest::getInt('operation', 0),
 					'signature' => $signature
 				)),
 			)
 		));
-
+		
 		$orderModel = VmModel::getModel('orders');
 		$order = $orderModel->getOrder($virtuemart_order_id);
-
+		
 		$repeat = 3;
 		try {
 			while ($repeat) {
 				$repeat--;
 				sleep(2);
-
+				
 				$xml = file_get_contents($url, false, $context);
 				if (!$xml)
 					throw new Exception("Empty data");
@@ -334,53 +338,55 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 				$response = json_decode(json_encode($xml), true);
 				if (!$response)
 					throw new Exception("Non valid XML was received");
-
+				
 				$tmp_response = (array)$response;
-				unset($tmp_response["signature"]);
+				unset($tmp_response['signature'], $tmp_response['ofd_state']);
 				$signature = base64_encode(md5(implode('', $tmp_response) . $method->password));
 				if ($signature !== $response['signature'])
 					throw new Exception("Invalid signature");
-
-				if (($response['type'] != 'PURCHASE' && $response['type'] != 'EPAYMENT') || $response['state'] != 'APPROVED')
+				
+				if (($response['type'] !== 'PURCHASE' && $response['type'] !== 'PURCHASE_BY_QR' && $response['type'] !== 'AUTHORIZE') || $response['state'] != 'APPROVED')
 					continue;
-
+				
 				$totalInPaymentCurrency = plgVmPaymentBest2Pay::getAmountInCurrency($order['details']['BT']->order_total, $method->payment_currency);
 				$totalInPaymentCurrency = $totalInPaymentCurrency['value'];
 				if ($response['amount'] != intval($totalInPaymentCurrency * 100) || $response['amount'] <= 0)
 					throw new Exception("Invalid price");
-
-				$order['order_status'] = 'C';
-				$orderModel->updateStatusForOneOrder($virtuemart_order_id, $order, TRUE);
+				
+				if ($order['order_status'] != 'C'){
+					$order['order_status'] = 'C';
+					$orderModel->updateStatusForOneOrder($virtuemart_order_id, $order, TRUE);
+				}
 				$cart = VirtueMartCart::getCart();
 				$cart->emptyCart();
-
+				
 				$html = '<table>' . "\n";
 				$html .= $this->getHtmlRow('BEST2PAY_ORDER_NUMBER', $order_number);
 				$html .= $this->getHtmlRow('BEST2PAY_ORDER_ID', $virtuemart_order_id);
 				$html .= $this->getHtmlRow('BEST2PAY_PAYMENT_ID', $response['id']);
 				$html .= $this->getHtmlRow('BEST2PAY_STATUS', JText::_('VMPAYMENT_BEST2PAY_SUCCESS'));
 				$html .= '</table>' . "\n";
-
+				
 				return true;
 			}
-
+			
 			throw new Exception('Unknown error');
-
+			
 		} catch (Exception $ex) {
 			error_log($ex->getMessage());
-			JError::raiseWarning(500, JText::_('VMPAYMENT_BEST2PAY_ERROR', false));
 			$mainframe = JFactory::getApplication();
+			$mainframe->enqueueMessage(JText::_('VMPAYMENT_BEST2PAY_ERROR', false), 'warning');
 			$mainframe->redirect(JURI::root() . 'index.php?option=com_virtuemart&view=cart');
 			return false;
 		}
 	}
-
+	
 	function plgVmOnUserPaymentCancel () {
-
+		
 		if (!class_exists ('VirtueMartModelOrders')) {
 			require(VMPATH_ADMIN . DS . 'models' . DS . 'orders.php');
 		}
-
+		
 		$order_number = vRequest::getString ('on', '');
 		$virtuemart_paymentmethod_id = vRequest::getInt ('pm', '');
 		if (empty($order_number) or
@@ -389,29 +395,29 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		) {
 			return NULL;
 		}
-
+		
 		if (!($virtuemart_order_id = VirtueMartModelOrders::getOrderIdByOrderNumber ($order_number))) {
 			return NULL;
 		}
-
+		
 		if (!($paymentTable = $this->getDataByOrderId ($virtuemart_order_id))) {
 			return NULL;
 		}
-
+		
 		$session = JFactory::getSession ();
 		$return_context = $session->getId ();
 		if (strcmp ($paymentTable->user_session, $return_context) === 0) {
-			//TODO $this->handlePaymentUserCancel ($virtuemart_order_id);
+			$this->handlePaymentUserCancel ($virtuemart_order_id);
 		}
-
+		
 		return TRUE;
 	}
-
+	
 	public function plgVmOnPaymentNotification() {
 		$lang = JFactory::getLanguage();
 		$filename = 'com_virtuemart';
 		$lang->load($filename, JPATH_ADMINISTRATOR);
-
+		
 		if (!class_exists('shopFunctionsF')) {
 			require(JPATH_VM_SITE . DS . 'helpers' . DS . 'shopfunctionsf.php');
 		}
@@ -427,10 +433,10 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		if (!class_exists('VirtueMartCart')) {
 			require(JPATH_VM_SITE . DS . 'helpers' . DS . 'cart.php');
 		}
-
-		$virtuemart_paymentmethod_id = JRequest::getInt('pm', 0);
-		$order_number = JRequest::getString('reference', 0);
-
+		
+		$virtuemart_paymentmethod_id = vRequest::getInt('pm', 0);
+		$order_number = vRequest::getString('reference', 0);
+		
 		if (!($method = $this->getVmPluginMethod($virtuemart_paymentmethod_id))) {
 			return NULL;
 		}
@@ -455,26 +461,28 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		$response = json_decode(json_encode($xml), true);
 		if (!$response)
 			die('error 3');
-
+		
 		header('Content-type: text/plain');
-
-		if (($response['type'] != 'PURCHASE' && $response['type'] != 'EPAYMENT') || $response['state'] != 'APPROVED')
+		
+		if (($response['type'] !== 'PURCHASE' && $response['type'] !== 'PURCHASE_BY_QR' && $response['type'] !== 'AUTHORIZE') || $response['state'] != 'APPROVED')
 			die('error 4');
-
+		
 		$signature = $response['signature'];
 		unset($response['signature']);
 		$str = implode('', (array)$response) . $method->password;
 		$my_signature = base64_encode(md5($str));
 		if ($my_signature !== $signature)
 			die('error 4');
-
+		
 		$totalInPaymentCurrency = plgVmPaymentBest2Pay::getAmountInCurrency($order['details']['BT']->order_total, $method->payment_currency);
 		$totalInPaymentCurrency = $totalInPaymentCurrency['value'];
 		if ($response['amount'] != intval($totalInPaymentCurrency * 100) || $response['amount'] <= 0)
 			die('error 5');
-
-		$order['order_status'] = 'C';
-		$orderModel->updateStatusForOneOrder($virtuemart_order_id, $order, TRUE);
+		
+		if ($order['order_status'] != 'C'){
+			$order['order_status'] = 'C';
+			$orderModel->updateStatusForOneOrder($virtuemart_order_id, $order, TRUE);
+		}
 		
 		die('ok');
 	}
@@ -483,5 +491,5 @@ class plgVmPaymentBest2Pay extends vmPSPlugin {
 		return JURI::root() . "index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&pm="
 			. $order['details']['BT']->virtuemart_paymentmethod_id;
 	}
-
+	
 }
